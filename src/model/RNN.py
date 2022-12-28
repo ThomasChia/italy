37baa2a1588ba711a2ee0424925a814d0d94d2c6
Thomas Chia <tchia97@gmail.com>
2022-12-28 17:45:08 +0000

    Update ignore file.

---
+++ src/model/RNN.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+import torch.functional as F
+
+
+class RNN(nn.Module):
+
+    def __init__(self, input_size, output_size, hidden_dim, n_layers):
+        super(Model, self).__init__()
+
+        # Defining some parameters
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        #Defining the layers
+        # RNN Layer
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_size)
+
+    def forward(self, x):
+
+        batch_size = x.size(0)
+
+        # Initializing hidden state for first input using method defined below
+        hidden = self.init_hidden(batch_size)
+
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.rnn(x, hidden)
+        
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        out = out.contiguous().view(-1, self.hidden_dim)
+        out = self.fc(out)
+        
+        return out, hidden
