746399a339b896ea8c17ea19d4af8820a60b3c1e
Thomas Chia <tchia97@gmail.com>
2022-12-30 14:20:18 +0000

    Add in model notebooks.

--- src/preprocessing/join_data.py
+++ src/preprocessing/join_data.py
@@ -2,26 +2,42 @@ import numpy as np
 import pandas as pd
 
 
-def load_past_matches(file):
-    df = pd.read_csv(f'../../data/{file}')
-    df.drop('Unnamed: 0', axis=1, inplace=True)
-    df['date'] = pd.to_datetime(df['date'], utc = True)
+class Loader:
 
-    return df
+    def __init__(self, files):
+        self.files = files
 
+    def get_data(self):
+        dfs = []
+        for file in self.files:
+            df = self.load_past_matches(file)
+            dfs.append(df)
+        
+        df_join = self.join_data(dfs[0], dfs[1])
 
-def join_data(df1, df2):
-    df = pd.merge(df1, df2,  how='left',
-        left_on=['league', 'date','team', 'opponent', 'home'],
-        right_on=['league', 'date','team', 'opponent', 'home'])
-    df.sort_values(by=['date', 'league', 'team', 'opponent'], inplace=True)
+        return df_join
 
-    return df
+    def load_past_matches(self, file):
+        df = pd.read_csv(f'../../data/{file}')
+        df.drop('Unnamed: 0', axis=1, inplace=True)
+        df['date'] = pd.to_datetime(df['date'], utc = True)
 
+        return df
 
-elos = load_past_matches("elos_matches.csv")
-goals = load_past_matches("goals_matches.csv")
-data = join_data(elos, goals)
+    def join_data(self, df1, df2):
+        df = pd.merge(df1, df2,  how='left',
+            left_on=['league', 'date','team', 'opponent', 'home'],
+            right_on=['league', 'date','team', 'opponent', 'home'])
+        df.sort_values(by=['date', 'league', 'team', 'opponent'], inplace=True)
 
-# print(data.shape)
-# print(data.tail())
+        return df
+
+FILES = ["elos_matches.csv", "goals_matches.csv"]
+loader = Loader(FILES)
+data = loader.get_data()
+# elos = load_past_matches("elos_matches.csv")
+# goals = load_past_matches("goals_matches.csv")
+# data = join_data(elos, goals)
+
+print(data.shape)
+print(data.tail())
