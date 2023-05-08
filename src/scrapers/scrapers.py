5c1a0569f4587ed52800a5e687180e89c3305dc1
Thomas Chia <tchia97@gmail.com>
2023-05-08 17:02:31 +0100

    Update to record year in date when scraping.

--- src/scrapers/scrapers.py
+++ src/scrapers/scrapers.py
@@ -43,22 +43,34 @@ class FlashScoreScraper(Scraper):
             self.site_active = False
 
     def get_fixture_data(self, league):
-        times, home_teams, away_teams = self.get_fixture_elements()
-        self.store_future_matches(times, home_teams, away_teams, league)
+        times, years, home_teams, away_teams = self.get_fixture_elements()
+        self.store_future_matches(times, years, home_teams, away_teams, league)
 
     def get_fixture_elements(self):
         body = self.driver.find_element(By.XPATH, '//*[@id="live-table"]/div[1]/div/div')
+        years = body.find_elements(By.XPATH, '//*[@id="mc"]/div[4]/div[1]/div[2]/div[2]')[0].text
         times = body.find_elements(By.CSS_SELECTOR, "div.event__time")
         home_teams = body.find_elements(By.CSS_SELECTOR, "div.event__participant.event__participant--home")
         away_teams = body.find_elements(By.CSS_SELECTOR, "div.event__participant.event__participant--away")
 
-        return times, home_teams, away_teams
+        return times, years, home_teams, away_teams
     
-    def store_future_matches(self, times, home_teams, away_teams, league):
+    def store_future_matches(self, times, years, home_teams, away_teams, league):
         for ind in range(len(times)):
-            self.matches.matches_dict['date'].append(times[ind].text)
+            date = self.get_date(times, years, ind)
+            self.matches.matches_dict['date'].append(date)
             self.matches.matches_dict['league'].append(league)
             self.matches.matches_dict['pt1'].append(home_teams[ind].text)
             self.matches.matches_dict['pt2'].append(away_teams[ind].text)
 
         self.matches.matches_df = pd.DataFrame(self.matches.matches_dict)
+
+    def get_date(self, times, years, ind):
+        first_year = years.split('/')[0]
+        second_year = years.split('/')[1]
+        day_month = times[ind].text.split(' ', 1)[0]
+        month = day_month.split('.')[1]
+        if int(month) >= 7:
+            return f"{day_month}{first_year}"
+        else:
+            return f"{day_month}{second_year}"
