0df0493c50ffa2627a5d1e1673a6c409037c5d47
Thomas Chia <tchia97@gmail.com>
2023-06-11 17:40:18 +0100

    Update logging to logger statements.

--- src/planners/in_season.py
+++ src/planners/in_season.py
@@ -35,9 +35,9 @@ class InSeasonPlanner(Planner):
         """
         Run the in-season planner.
         """
-        logging.info("Running in-season planner.")
+        logger.info("Running in-season planner.")
 
-        logging.info("Loading data.")
+        logger.info("Loading data.")
         query = Query()
         query.leagues_specific(config.TABLE_NAME_PAST, config.LEAGUES, config.COUNTRIES)
         loader = DBConnector()
@@ -45,15 +45,15 @@ class InSeasonPlanner(Planner):
         if debug:
             loader.data = loader.data[loader.data['date']>='2020-08-01']
 
-        logging.info("Scraping future matches.")
+        logger.info("Scraping future matches.")
         future_matches = MultiScraper(config.COUNTRIES)
         future_matches.scrape_all()
 
-        logging.info("Scraping 538 predictions.")
+        logger.info("Scraping 538 predictions.")
         scraper = FiveThirtyEightScraper()
         scraper.run()
 
-        logging.info("Cleaning data.")
+        logger.info("Cleaning data.")
         cleaner = Cleaner(loader)
         cleaner.clean()
 
@@ -61,23 +61,23 @@ class InSeasonPlanner(Planner):
         validator = ValidateMatches(past_matches = cleaner.data, future_matches = future_matches.scraped_matches, season_start=config.SEASON_START, season_end=config.SEASON_END)
         validator.run()
 
-        logging.info("Storing past matches.")
+        logger.info("Storing past matches.")
         past_matches = PastMatches(cleaner.data)
 
         # TODO remove duplicates at each stage of the data pipeline. Can add in match_id to help this, as can always just remove duplicates with the same match_id.
-        logging.info("Calculating elo statistics.")
+        logger.info("Calculating elo statistics.")
         elos = EloPreprocessor(cleaner.data)
         elos.calculate_elos()
 
-        logging.info("Calculating goals statistics.")
+        logger.info("Calculating goals statistics.")
         goals = GoalsPreprocessor(cleaner.data)
         goals.calculate_goals_statistics()
 
-        logging.info("Building training set.")
+        logger.info("Building training set.")
         builder = Builder([elos, goals])
         builder.build_dataset()
 
-        logging.info("Building prediction set.")
+        logger.info("Building prediction set.")
         future_builder = FutureBuilder(future_matches.scraped_matches, builder)
         future_builder.build_future_matches()
 
@@ -85,23 +85,23 @@ class InSeasonPlanner(Planner):
         adjustor = ManualAdjustor()
         future_builder.preprocessed_future_matches = adjustor.run(future_builder.preprocessed_future_matches)
 
-        logging.info("Training model.")
+        logger.info("Training model.")
         model = Model(builder.data)
         model.train()
 
-        logging.info(f"Predicting {future_builder.preprocessed_future_matches.shape[0]} matches.") # TODO add in number of matches being predicted.
+        logger.info(f"Predicting {future_builder.preprocessed_future_matches.shape[0]} matches.") # TODO add in number of matches being predicted.
         future_home_and_away_matches, future_team_and_opponent = model.predict(future_builder.preprocessed_future_matches, config.FEATURES, config.ID_FEATURES)
         # TODO add in something to tell which league we are looking at.
 
-        logging.info("Running simulations.")
+        logger.info(f"Running {config.NUM_SIMULATIONS} simulations.")
         simulator = MonteCarloSimulator(future_home_and_away_matches)
         simulation_results = simulator.run_simulations(num_simulations=config.NUM_SIMULATIONS)
 
-        logging.info("Creating output.")
+        logger.info("Creating output.")
         results = MonteCarloResults(simulation_results=simulation_results, past_results=deepcopy(past_matches), season_start=config.SEASON_START)
         results.get_finishing_positions()
 
-        logging.info("Writing output to db.")
+        logger.info("Writing output to db.")
         query = Query()
         query.read_last_future_predictions()
         loader = DBConnector()
@@ -109,7 +109,7 @@ class InSeasonPlanner(Planner):
         merged = pd.merge(loader.data, future_team_and_opponent, on=['match_id', 'team'], how='outer', suffixes=('_left', '_right'))
         diff = merged[merged['league_left'] != merged['league_right']]
 
-        logging.info("Postprocessing output for gsheets.")
+        logger.info("Postprocessing output for gsheets.")
         post_processor = InSeasonPostProcessor(league_targets=results.league_targets,
                                                results=past_matches,
                                                past_predictions=pd.DataFrame(),
@@ -122,4 +122,4 @@ class InSeasonPlanner(Planner):
 
         
 
-        logging.info("In-season planner complete.")
+        logger.info("In-season planner complete.")
